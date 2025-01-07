5a19fdf290cc53ca924255f8ccb6c76827e48e22
Ethan <ethansmithson413@gmail.com>
2025-01-06 21:00:07 -0500

    first commit

---
+++ linkedListPractice.py
@@ -0,0 +1,80 @@
+class Node(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+    
+    def get_data(self):
+        return self.val
+    
+    def set_data(self, val):
+        self.val = val
+
+    def get_next(self):
+        return self.next
+    
+    def set_next(self, next):
+        self.next = next
+
+    def get_list(self):
+        return self.val
+
+class linkedList(object):
+    def __init__(self, head = None):
+        self.head = head
+        self.count = 0
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+        self.count += 1
+
+    def find(self, val):
+        item = self.head
+        while item != None:
+            if item.get_data() == val:
+                return item.val
+            else :
+                item = item.get_next()
+        return None
+    
+    def remove(self, val):
+        current = self.head
+        previous = None
+        while current is not None:
+            if current.val == val:
+                break
+            previous = current
+            current = current.get_next()
+        
+        if current is None:
+            raise ValueError(f"{val} is not in the list.")
+        
+        if previous is None:
+            self.head = current.next
+            self.count -= 1
+        else:
+            previous.set_next(current.get_next())
+            self.count -= 1
+
+    def get_count(self):
+        return self.count
+    
+    def is_empty(self):
+        return self.head == None
+    
+    def show_data(self):
+        current = self.head
+        while current is not None:
+            print(current.val)
+            current = current.next
+
+
+
+list = linkedList()
+list.insert(15)
+list.insert("test")
+list.insert(123)
+list.remove("test")
+# print(list.get_count())
+print(list.show_data())
